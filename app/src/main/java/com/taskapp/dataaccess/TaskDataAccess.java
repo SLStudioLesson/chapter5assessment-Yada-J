12bf9161c028ce8b7e4a86c7a8ab6e84535ab059
Yada-J <Yada.Junki@technopro.com>
2025-01-14 12:31:53 +0900

    解答完了

--- app/src/main/java/com/taskapp/dataaccess/TaskDataAccess.java
+++ app/src/main/java/com/taskapp/dataaccess/TaskDataAccess.java
@@ -1,5 +1,16 @@
 package com.taskapp.dataaccess;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.taskapp.model.Task;
+import com.taskapp.model.User;
+
 public class TaskDataAccess {
 
     private final String filePath;
@@ -27,52 +38,111 @@ public class TaskDataAccess {
      * @see com.taskapp.dataaccess.UserDataAccess#findByCode(int)
      * @return タスクのリスト
      */
-    // public List<Task> findAll() {
-    //     try () {
+    public List<Task> findAll() {
+        List<Task> tasks = new ArrayList<Task>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            // タイトル行を読み飛ばす
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
 
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    //     return null;
-    // }
+                // CSVに間違いがあったらスキップする
+                if (values.length != 4) {
+                    continue;
+                }
+
+                int code = Integer.parseInt(values[0]);
+                String name = values[1];
+                int status = Integer.parseInt(values[2]);
+                User repUser = userDataAccess.findByCode(Integer.parseInt(values[3]));
+
+                // Taskオブジェクトにマッピングしていく
+                Task task = new Task(code, name, status, repUser);
+                // tasksに追加する
+                tasks.add(task);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
+
 
     /**
      * タスクをCSVに保存します。
      * @param task 保存するタスク
      */
-    // public void save(Task task) {
-    //     try () {
-
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+    public void save(Task task) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath, true))) {
+            String line = createLine(task);
+            // 改行を追加する
+            writer.newLine();
+            // データを1行分追加する
+            writer.write(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * コードを基にタスクデータを1件取得します。
      * @param code 取得するタスクのコード
      * @return 取得したタスク
      */
-    // public Task findByCode(int code) {
-    //     try () {
+    public Task findByCode(int code) {
+        Task task = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            // タイトル行を読み飛ばす
+            reader.readLine();
+            while ((line = reader.readLine()) != null) {
+                String[] values = line.split(",");
 
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    //     return null;
-    // }
+                int taskCode = Integer.parseInt(values[0]);
+
+                if (code != taskCode) continue;
+
+                String name = values[1];
+                int status = Integer.parseInt(values[2]);
+                User repUser = userDataAccess.findByCode(Integer.parseInt(values[3]));
+
+                // Taskオブジェクトにマッピングしていく
+                task = new Task(code, name, status, repUser);
+                break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return task;
+    }
 
     /**
      * タスクデータを更新します。
      * @param updateTask 更新するタスク
      */
-    // public void update(Task updateTask) {
-    //     try () {
+    public void update(Task updateTask) {
+        List<Task> tasks = findAll();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            // CSVのヘッダーを書き込む
+            writer.write("Code,Name,Status,Rep_User_Code\n");
 
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
+            String line;
+            for (Task task : tasks) {
+                // 編集対象のLibraryなら、updateLibraryの情報を書き込む
+                if (task.getCode() == updateTask.getCode()) {
+                    line = createLine(updateTask);
+                } else {
+                    line = createLine(task);
+                }
+                writer.write(line);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * コードを基にタスクデータを削除します。
@@ -91,6 +161,7 @@ public class TaskDataAccess {
      * @param task フォーマットを作成するタスク
      * @return CSVに書き込むためのフォーマット文字列
      */
-    // private String createLine(Task task) {
-    // }
+    private String createLine(Task task) {
+        return task.getCode() + "," + task.getName() + "," + task.getStatus() + "," + task.getRepUser().getCode();
+    }
 }
--- app/src/main/java/com/taskapp/logic/TaskLogic.java
+++ app/src/main/java/com/taskapp/logic/TaskLogic.java
@@ -1,8 +1,15 @@
 package com.taskapp.logic;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.taskapp.dataaccess.LogDataAccess;
 import com.taskapp.dataaccess.TaskDataAccess;
 import com.taskapp.dataaccess.UserDataAccess;
+import com.taskapp.exception.AppException;
+import com.taskapp.model.Log;
+import com.taskapp.model.Task;
+import com.taskapp.model.User;
 
 public class TaskLogic {
     private final TaskDataAccess taskDataAccess;
@@ -34,8 +41,29 @@ public class TaskLogic {
      * @see com.taskapp.dataaccess.TaskDataAccess#findAll()
      * @param loginUser ログインユーザー
      */
-    // public void showAll(User loginUser) {
-    // }
+    public void showAll(User loginUser) {
+        //// findAllメソッドを実行して、データの一覧を取得
+        List<Task> tasks = taskDataAccess.findAll();
+        // 取得したデータを表示する
+        tasks.forEach(task -> {
+            // 以下の条件分岐を追加
+            String status = "完了";
+            if (task.getStatus() == 1) {
+                status = "着手中";
+            } else if (task.getStatus() == 0) {
+                status = "未着手";
+            }
+
+            String charge = "あなたが担当しています";
+            if (task.getRepUser().getCode() != loginUser.getCode()) {
+                charge = task.getRepUser().getName() + "が担当しています";
+            }
+
+            System.out.println(task.getCode() + ". " + "タスク名：" + task.getName() +
+                    ", 担当者名：" + charge + ", ステータス：" + status);
+        });
+
+    }
 
     /**
      * 新しいタスクを保存します。
@@ -49,9 +77,23 @@ public class TaskLogic {
      * @param loginUser ログインユーザー
      * @throws AppException ユーザーコードが存在しない場合にスローされます
      */
-    // public void save(int code, String name, int repUserCode,
-    //                 User loginUser) throws AppException {
-    // }
+    public void save(int code, String name, int repUserCode,
+                    User loginUser) throws AppException {
+
+        User user = userDataAccess.findByCode(repUserCode);
+        if (user == null) {
+            throw new AppException("存在するユーザーコードを入力してください");
+        }
+        // 入力値をTaskオブジェクトにマッピング
+        Task task = new Task(code, name, 0, user);
+        taskDataAccess.save(task);
+        // 新しくLogオブジェクトを生成する
+        Log log = new Log(code, loginUser.getCode(), 0, LocalDate.now());
+        // logs.csvにデータを一件新規登録する
+        logDataAccess.save(log);
+        System.out.println( name + "の登録が完了しました。");
+
+    }
 
     /**
      * タスクのステータスを変更します。
@@ -64,9 +106,18 @@ public class TaskLogic {
      * @param loginUser ログインユーザー
      * @throws AppException タスクコードが存在しない、またはステータスが前のステータスより1つ先でない場合にスローされます
      */
-    // public void changeStatus(int code, int status,
-    //                         User loginUser) throws AppException {
-    // }
+    public void changeStatus(int code, int status,
+                            User loginUser) throws AppException {
+        Task task = taskDataAccess.findByCode(code);
+
+        if (task == null) {
+            throw new AppException("存在するタスクコードを入力してください");
+        }
+        if (library.getRentUserCode() != 0) {
+            throw new AppException("ステータスは、前のステータスより1つ先のもののみを選択してください");
+        }
+
+    }
 
     /**
      * タスクを削除します。
